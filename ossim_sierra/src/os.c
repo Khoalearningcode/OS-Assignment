f8ded9e55614b9307f923c5855831a96ba4ea1e0
Quan-Quasar-Trinh <billynhatquan@gmail.com>
2025-04-26 17:28:23 +0700

    test2

--- ossim_sierra/src/os.c
+++ ossim_sierra/src/os.c
@@ -188,7 +188,6 @@ static void read_config(const char * path) {
 		char proc[100];
 #ifdef MLQ_SCHED
 		fscanf(file, "%lu %s %lu\n", &ld_processes.start_time[i], proc, &ld_processes.prio[i]);
-		printf("%s", proc);
 #else
 		fscanf(file, "%lu %s\n", &ld_processes.start_time[i], proc);
 #endif
@@ -207,11 +206,7 @@ int main(int argc, char * argv[]) {
 	strcat(path, "input/");
 	strcat(path, argv[1]);
 	read_config(path);
-	for(int i=0; i<num_processes; i++)
-	{
-		printf("%s\n",ld_processes.path[i] );
-	}
-
+	
 	pthread_t * cpu = (pthread_t*)malloc(num_cpus * sizeof(pthread_t));
 	struct cpu_args * args =
 		(struct cpu_args*)malloc(sizeof(struct cpu_args) * num_cpus);
